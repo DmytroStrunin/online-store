11be84da61add1f0e15ff711766e654e83fa46ff
Dmytro Strunin <strunin.dmytro@gmail.com>
2022-10-26 16:58:07 +0300

    flyway scripts

--- src/main/resources/db/migration/V3.1__Add_manufacturers.sql
+++ src/main/resources/db/migration/V3.1__Add_manufacturers.sql
@@ -1,9 +1,9 @@
 insert into manufacturers (country, image, name, manufacturer_id)
-values ('Korea', 'https://i.ibb.co/bRP3yGF/Samsung-wordmark-svg.png', 'Samsung', '7e6808da-944d-4bf1-b6ab-86e9176f9c1e');
-
-insert into manufacturers (country, image, name, manufacturer_id)
 values ('USA', 'https://i.ibb.co/TK4D03s/Apple-logo-black-svg.webp', 'Apple', '9452abb0-78d6-4c8b-80e5-11adac34278d');
 
 insert into manufacturers (country, image, name, manufacturer_id)
 values ('Taiwan', 'https://i.ibb.co/jJ1w4N7/ASUS-Corporate-Logo-svg.png', 'Asus', '622e454b-d383-42fd-86aa-6f21b1c1dc09');
+
+insert into manufacturers (country, image, name, manufacturer_id)
+values ('Korea', 'https://i.ibb.co/bRP3yGF/Samsung-wordmark-svg.png', 'Samsung', '7e6808da-944d-4bf1-b6ab-86e9176f9c1e');
 
